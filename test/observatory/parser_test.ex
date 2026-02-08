885c4c5a0c2466378d852cf5434b5415145e22d2
Ongera <ongeradaryn@gmail.com>
2026-02-09 01:37:05 +0300

    chore: test stream data parsing

--- test/observatory/parser_test.ex
+++ test/observatory/parser_test.ex
@@ -92,4 +92,56 @@ defmodule Observatory.ParserTest do
       assert schema.format.metadata == %{}
     end
 
+  test "parses video stream correctly" do
+      {:ok, schema} = FFprobeParser.parse(@sample_mp4_output)
+
+      video_stream = Enum.find(schema.streams, &(&1.type == :video))
+
+      assert video_stream.index == 0
+      assert video_stream.type == :video
+      assert video_stream.codec_name == "h264"
+      assert video_stream.codec_profile == "High"
+      assert video_stream.timebase == {1, 90000}
+      assert video_stream.duration_sec == 120.5
+      assert video_stream.bitrate_bps == 2_100_000
+
+      # Video-specific fields
+      assert video_stream.width == 1920
+      assert video_stream.height == 1080
+      assert video_stream.frame_rate == {30, 1}
+      assert video_stream.pixel_format == "yuv420p"
+      assert video_stream.color_space == "bt709"
+      assert video_stream.color_range == "tv"
+
+      # Audio fields should be nil
+      refute video_stream.sample_rate
+      refute video_stream.channels
+      refute video_stream.channel_layout
+    end
+
+    test "parses audio stream correctly" do
+      {:ok, schema} = FFprobeParser.parse(@sample_mp4_output)
+
+      audio_stream = Enum.find(schema.streams, &(&1.type == :audio))
+
+      assert audio_stream.index == 1
+      assert audio_stream.type == :audio
+      assert audio_stream.codec_name == "aac"
+      assert audio_stream.codec_profile == "LC"
+      assert audio_stream.timebase == {1, 48000}
+      assert audio_stream.duration_sec == 120.48
+      assert audio_stream.bitrate_bps == 128_000
+
+      # Audio-specific fields
+      assert audio_stream.sample_rate == 48000
+      assert audio_stream.channels == 2
+      assert audio_stream.channel_layout == "stereo"
+
+      # Video fields should be nil
+      refute audio_stream.width
+      refute audio_stream.height
+      refute audio_stream.frame_rate
+      refute audio_stream.pixel_format
+    end
+
 end
