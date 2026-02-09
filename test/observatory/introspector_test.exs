da54c229fc854fbdab509bb0ee879c25de16ce09
Ongera <ongeradaryn@gmail.com>
2026-02-09 12:18:02 +0300

    test: Observatory.Introspector tests

---
+++ test/observatory/introspector_test.exs
@@ -0,0 +1,25 @@
+defmodule Observatory.IntrospectorTest do 
+  use ExUnit.Case, async: true
+
+  alias Observatory.Introspector 
+  alias Observatory.MediaSchema
+  alias Observatory.MediaSchema.{Format, Stream}
+
+  describe "tests for analyze/1" do
+    test "test analyze/1 with valid file path" do
+      assert {:ok, _} = Introspector.analyze("/home/ongera/Music/adjusted_rap_vocals.wav")
+    end
+
+    test "test analyze/1 with invalid path" do
+      assert {:error, _} = Introspector.analyze("/home/ongera/Music/rap_vocals.wav")
+    end
+  end
+
+  describe "test for ffbrobe related functions" do
+    test "test ffprobe_available?/1" do
+      assert :true = Introspector.ffprobe_available?(["-version"])
+    end 
+  end
+
+
+end
